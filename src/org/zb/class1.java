e8ad78f2ebc397afdb2b7cf44ff26a19eaf4760b
GoSpursGo2017 <415170809@qq.com>
2017-01-25 22:33:25 +0800

    Get it, and update from GT to New Master 2

--- src/org/zb/class1.java
+++ src/org/zb/class1.java
@@ -10,6 +10,8 @@ public class class1 {
 		System.out.println("1111");
 		System.out.println("3333");
 		System.out.println("Update stream from local-New Master");
+		System.out.println("Get it, and update from GT into New master 2");
+
 		
 
 		
