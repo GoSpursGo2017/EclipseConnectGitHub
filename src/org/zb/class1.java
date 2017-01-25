fa2a09c4153b2467cb06ecf0e51dc2faf114e9b3
GoSpursGo2017 <415170809@qq.com>
2017-01-25 23:55:16 +0800

    Fetch from upstream example

--- src/org/zb/class1.java
+++ src/org/zb/class1.java
@@ -9,7 +9,7 @@ public class class1 {
 		System.out.println("newBranch");
 		System.out.println("1111");
 		System.out.println("3333");
-		System.out.println("Let's to test upstream");
+		System.out.println("Fetch from upstream example");
 
 		
 	}
